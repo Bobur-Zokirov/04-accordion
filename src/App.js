053624a864e1eb6cfa067dae15abe7442f4ff4fb
Bobur Zokirov <zeromegamaks@gmail.com>
2023-06-27 20:39:56 +0500

    04-accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import questions from "./constants/data";
 
-function App() {
+import Question from "./Question";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>Questions and Answers about Login</h3>
+        <div>
+          {questions.map((question) => {
+            return <Question key={question.id} {...question} />;
+          })}
+        </div>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
